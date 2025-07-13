b13da9f47a3099faa2386cfcab854f63b761850a
Gautham Kishtapuram <skishtapuram@loyaltymethods.com>
2025-07-13 21:01:29 +0530

    solved two sum 2 using two pointer approch ref : https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/

--- src/main/java/org/implementations/Leetcode167.java
+++ src/main/java/org/implementations/Leetcode167.java
@@ -22,9 +22,29 @@ public class Leetcode167 {
         return null;
     }
 
+    public int[] twoSumOptimal(int[] numbers, int target) {
+        int leftPointer = 0;
+        int rightPointer = numbers.length - 1;
+        while (leftPointer < rightPointer) {
+            int sum = numbers[leftPointer] + numbers[rightPointer];
+            if (sum == target) {
+                return new int[]{++leftPointer, ++rightPointer};
+            } else if (sum > target) {
+                rightPointer--;
+            } else {
+                leftPointer++;
+            }
+        }
+        return null;
+    }
+
     public static void main(String[] args) {
         Leetcode167 leetcode167 = new Leetcode167();
         int[] result = leetcode167.twoSum(new int[]{2, 3, 4, 5, 6, 7}, 9);
+
+        int[] resultOpt = leetcode167.twoSumOptimal(new int[]{2, 3, 4, 5, 6, 7}, 9);
+
         System.out.println(Arrays.toString(result));
+        System.out.println(Arrays.toString(resultOpt));
     }
 }
